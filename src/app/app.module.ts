447c116126f9b484438d4c280a3fd4568648bba4
Carlos Olivos <carlosand.op@gmail.com>
2017-10-30 22:24:58 -0500

    Linea base Tour de heroes

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
